6b553dc4e2d206b7a9ce6e7124e94ee78e18a28b
Chebrolu Tejaswi <tejaswichebrolu@gmail.com>
2022-03-09 12:13:15 +0530

    added level-1 security: success page is restricted only to registered users

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const res = require("express/lib/response");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.static("public"));
@@ -9,6 +10,17 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({
     extended : true
 }));
+
+// Connecting our database
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
+// User Schema
+const userSchema={
+    email:String,
+    password: String
+};
+// Model
+const User=new mongoose.model("User",userSchema);
+
 // ---------------------------------------------
 
 app.get("/",function(req,res){
@@ -17,14 +29,58 @@ app.get("/",function(req,res){
 app.get("/login",function(req,res){
     res.redirect("/");
 });
+app.post("/login",function(req,res){
+    const username= req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username},function(err,foundUser){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            if(foundUser)
+            {
+                if(foundUser.password === password)
+                {
+                    res.render("success");
+                }
+                else
+                {
+                    res.redirect("/");
+                }
+            }
+            else
+            {
+                res.redirect("/");
+            }
+        }
+    });
+});
+
 app.get("/register",function(req,res){
     res.render("register");
 });
-
-app.get("/success",function(req,res){
-    res.render("success");
+app.post("/register",function(req,res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function(err){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.render("success");
+        }
+    });
 });
 
+app.get("/logout",function(req,res){
+    res.redirect("/");
+})
 // ---------------------------------------------
 
 app.listen(3000, function() {
